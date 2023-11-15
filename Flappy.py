18756c0a881a45eed96f4d0bed4d74c9c8cd0fae
yousra shleibik <143129587+YousraShleibik@users.noreply.github.com>
2023-11-15 14:53:08 -0700

    added images

--- Flappy.py
+++ Flappy.py
@@ -4,4 +4,10 @@ import pygame
 from pygame.locals import *
 import numpy
 import matplotlib.pyplot as plt
+import time
+import os
 
+WIN_WODTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMG= [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs",'bird1.png')))]
